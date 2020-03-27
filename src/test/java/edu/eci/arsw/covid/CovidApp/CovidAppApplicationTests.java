6ebab2e0355ef45f5212e22ecb09107b66b9be0b
nduran06 <natalia.duran@mail.escuelaing.edu.co>
2020-03-26 19:04:08 -0500

    readme

---
+++ src/test/java/edu/eci/arsw/covid/CovidApp/CovidAppApplicationTests.java
@@ -0,0 +1,11 @@
+package edu.eci.arsw.covid.CovidApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+//@SpringBootTest
+class CovidAppApplicationTests {
+
+	
+
+}
